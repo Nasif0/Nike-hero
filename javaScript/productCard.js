bd6409878451be6effe5a25a25fbba277b18ecc3
Nasif.0 <princenasif.00@gmail.com>
2023-07-04 14:50:56 +0600

    home carousel commit

---
+++ javaScript/productCard.js
@@ -0,0 +1,28 @@
+var displayImage = document.querySelector('#changeImage');
+
+function firstColour(){
+     displayImage.src = 'image/women-card2-colour1.png'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
